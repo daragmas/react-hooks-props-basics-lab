d0cb8cae74565d80c1c1946b8e4bb69a76f73dab
daragmas <daragmas@gmail.com>
2022-07-19 10:41:49 -0400

    commit tree f1d64eb420c5214b4fce0c976f969f6338a11ebd

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,23 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
+
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
+/*
+bio: "I made this!"
+city: "New York"
+color: "firebrick"
+links: {github: 'https://github.com/liza', linkedin: 'https://www.linkedin.com/in/liza/'}
+name: "Liza"*/
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} color={user.color}/>
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
